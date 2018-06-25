2c04de856dc9b27bb6de313607e31f88ad7ddd0c
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2018-06-25 17:43:27 +0530

    title changed

--- src/app/views/app/admissionForm.blade.php
+++ src/app/views/app/admissionForm.blade.php
@@ -16,7 +16,7 @@
         ===
     -->
     <meta charset="utf-8">
-    <title>School Manage</title>
+    <title>College Manage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
