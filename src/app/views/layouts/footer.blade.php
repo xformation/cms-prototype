31d00ec063d103d79bc86ece3fe84f88ad8980d3
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2018-06-26 12:27:13 +0530

    synectiks name corrected

--- src/app/views/layouts/footer.blade.php
+++ src/app/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         <p class="col-md-3 col-sm-3 col-xs-12 copyright"> <a href="#" target="_blank">{{Session::get('inName')}}</a> &copy;{{date('Y')}}</p>
         <p class="col-md-9 col-sm-9 col-xs-12 powered-by">
          <!-- Don't remove below text. Its againts copy right laws. -->
-         <strong>College Management System Version 1.0 - {{substr($idc,0,7)}}</strong> || Developed by <a class="cplink" href="http://syenctiks.com">Synectiks</a>
+         <strong>College Management System Version 1.0 - {{substr($idc,0,7)}}</strong> || Developed by <a class="cplink" href="http://synectiks.com">Synectiks</a>
         </p> 
   </footer>
   <!-- /footer content -->
